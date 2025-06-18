020c6b1cd32af33ccf9c9629e915d0d5214bdb58
Dongyue Yu <56863849+ydyydy@users.noreply.github.com>
2025-06-18 22:17:54 +0200

    feat: First commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { QuestionsController } from './questions/questions.controller';
+import { QuestionsService } from './questions/questions.service';
+import { ExamsController } from './exams/exams.controller';
+import { ExamsService } from './exams/exams.service';
+import { ReviewController } from './review/review.controller';
+import { ReviewService } from './review/review.service';
+import { ResultsController } from './results/results.controller';
+import { ResultsService } from './results/results.service';
+import { GamificationController } from './gamification/gamification.controller';
+import { GamificationService } from './gamification/gamification.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController, UsersController, QuestionsController, ExamsController, ReviewController, ResultsController, GamificationController],
+  providers: [AppService, AuthService, UsersService, QuestionsService, ExamsService, ReviewService, ResultsService, GamificationService],
+})
+export class AppModule {}
