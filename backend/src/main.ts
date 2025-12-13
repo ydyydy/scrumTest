505d8952b30eec924c46b29479c40a676b823358
Dongyue Yu <56863849+ydyydy@users.noreply.github.com>
2025-12-13 20:07:55 +0100

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { join } from 'path';
+import { Request, Response, NextFunction } from 'express';
+
 import * as express from 'express';
 import { AppModule } from './app.module';
 
@@ -7,19 +9,30 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // en producción, poner tu frontend real
+    origin: '*',
     methods: 'GET,POST,PUT,PATCH,DELETE',
     credentials: true,
   });
 
+  // 📦 React build
   const reactBuildPath = join(__dirname, '..', 'public');
   app.use(express.static(reactBuildPath));
 
-  // Middleware para React SPA
-  app.use((req: express.Request, res: express.Response) => {
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    if (
+      req.path.startsWith('/users') ||
+      req.path.startsWith('/questions') ||
+      req.path.startsWith('/reviews') ||
+      req.path.startsWith('/exams')
+    ) {
+      next();
+      return;
+    }
+
     res.sendFile(join(reactBuildPath, 'index.html'));
   });
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
