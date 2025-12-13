7eb12d3205390bbdf601d547b98cc8b7866d45d9
Dongyue Yu <56863849+ydyydy@users.noreply.github.com>
2025-12-13 16:41:11 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/app.css";
+import "./styles/App.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
